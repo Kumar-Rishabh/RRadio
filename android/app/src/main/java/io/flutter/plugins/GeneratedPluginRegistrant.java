7ca50eddc978bfc3a6353681e25f099d893a5f58
Kumar Rishabh <75268931+Kumar-Rishabh@users.noreply.github.com>
2021-06-02 22:26:25 +0530

    Add files via upload

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -4,6 +4,7 @@ import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 
 /**
  * Generated file. Do not edit.
@@ -13,6 +14,9 @@ import io.flutter.embedding.engine.FlutterEngine;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
+      app.alan.alan_voice.AlanVoicePlugin.registerWith(shimPluginRegistry.registrarFor("app.alan.alan_voice.AlanVoicePlugin"));
     flutterEngine.getPlugins().add(new bz.rxla.audioplayer.AudioplayerPlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.packageinfo.PackageInfoPlugin());
   }
 }
